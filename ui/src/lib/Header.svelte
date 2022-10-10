937f890bbf719442097de8390d5513b9bdb26510
Sanyia Saidova <sanyia.saidova@deliveroo.co.uk>
2022-10-10 22:05:51 +0100

    Making initial UI

---
+++ ui/src/lib/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+  const PromptuLogo = "Promptu";
+</script>
+
+<ul>
+  <li><a href="/">{PromptuLogo}</a></li>
+</ul>
+
+<style>
+  ul {
+    display: flex;
+    flex-direction: row;
+    justify-items: flex-start;
+    width: 100vw;
+    list-style-type: none;
+    margin: 0;
+    padding: 2em;
+    border-bottom: solid 3px whitesmoke;
+    font-size: 1.5em;
+    font-weight: bold;
+    background-color: #003566;
+    box-shadow: 0 0 1em 0 #1c2541;
+  }
+
+  a {
+    text-decoration: none;
+    color: whitesmoke;
+  }
+</style>
